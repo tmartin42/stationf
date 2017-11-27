a3c50d369c3158ddcc7ee1efeadc795182630c62
tmartin42 <japremieres2@gmail.com>
2017-11-27 20:22:30 +0100

    added brand new layout with controls

--- app/app.js
+++ app/app.js
@@ -12,7 +12,7 @@ config(['$locationProvider', '$routeProvider', function($locationProvider, $rout
 
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]).controller('appController', ['$scope', function($scope) {
-  $scope.view = 'none';
+  $scope.view = 'minimized';
   $scope.transition = function(){
     $scope.view = 'minimized';
   }
