adb2b3325eb34eab2f2bda0fd4f7c8a74470716b
tmartin42 <japremieres2@gmail.com>
2017-11-27 16:02:31 +0100

    first real commit. Base code and start of some css

---
+++ app/getjson/view1.js
@@ -0,0 +1,25 @@
+'use strict';
+
+angular.module('myApp.view1', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/view1', {
+    templateUrl: 'view1/view1.html',
+    controller: 'View1Ctrl'
+  });
+}])
+
+.controller('View1Ctrl', ['$scope', '$http', function($scope, $http) {
+  $scope.lol = 'lol';
+  var obj = {content:null};
+
+  $http.get('data/rooms.json').success(function(data) {
+    // you can do some processing here
+    obj.content = data;
+   // console.log(data);
+  });
+
+    $scope.showData = function() {
+      console.log(obj);
+    }
+}]);
