6843ce30e74dc5f1b0791ec809fd2b40e4f40466
tmartin42 <japremieres2@gmail.com>
2017-11-28 20:18:16 +0100

    enlevage du js superflu

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -18,27 +18,21 @@ angular.module('myApp.view1', ['ngRoute'])
   $scope.proj = false;
 
   $http.get('/data/rooms.json').success(function(data) {
-    // you can do some processing here
       $scope.rooms = data.rooms;
-   // console.log(data);
 
   });
 
     $scope.modalInit = function (index) {
         $scope.selected = $scope.rooms[index];
-
-    }
-
-
-    $scope.clear = function(model){
-
     };
+
+
 
     $scope.greaterThan = function(prop, val){
         return function(item){
             return Number(item[prop]) >= Number(val);
         }
-    }
+    };
 
     $scope.equfilter = function(){
         return function (rooms) {
@@ -56,15 +50,13 @@ angular.module('myApp.view1', ['ngRoute'])
 
     $scope.showData = function() {
       console.log($scope.rooms);
-    }
+    };
     $('#datetimepicker1').datetimepicker({
         format: 'YYYY-MM-DD HH:mm',
         stepping: 30,
         minDate: moment()
-    });/*
-    $('#datetimepicker1').on('change.datetimepicker',function(){
-        $('#date').trigger('input');
-    })*/
+    });
+
 
     $scope.checkbox = function(model){
         if ($scope[model])
@@ -73,31 +65,7 @@ angular.module('myApp.view1', ['ngRoute'])
             $scope[model] = true;
     };
 
-    $scope.test = function(model){
-        $scope.message = "Prise de rendez-vous reussie ! <br/> Elle sera lerfgdssd fgdsfgdsfgdfsgsdg " ;
-        $('.message').removeClass('above')
-        setTimeout(function () {
-            $('.message').addClass('success');
-        }, 100);
-        setTimeout(function () {
-            $('.success').removeClass('success').addClass('above');
-        }, 3100);
-    };
-    $scope.testt = function(){
-        $scope.message = "Erreur: Date invalide";
-        $('.message').removeClass('above');
-        setTimeout(function () {
-            $('.message').addClass('error');
-        }, 100);
-        setTimeout(function () {
-            $('.error').removeClass('error').addClass('above');
-        }, 3100);
-    };
-    $scope.$watch('datepicker'), function(newVal, oldVal, scope){
-        if(newVal != oldVal){
-            scope.datepicker = newVal;
-        }
-    };
+
     $scope.submitBooking = function() {
 
         var data = $('#date').val();
