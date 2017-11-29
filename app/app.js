60acf1e11ef2a542bdb32aea0ff15e16a8f2505e
tmartin42 <japremieres2@gmail.com>
2017-11-29 11:19:14 +0100

    fix du routing casse

--- app/app.js
+++ app/app.js
@@ -2,17 +2,18 @@
 
 // Declare app level module which depends on views, and components
 var app = angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+    'ngRoute',
+    'myApp.view1',
+    'myApp.view2',
+    'myApp.version'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+    $locationProvider.hashPrefix('!');
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
+    $routeProvider.otherwise({redirectTo: '/view1'});
 }]).controller('appController', ['$scope', function($scope) {
 
-  $scope.transition = function(){
-    $('.menu').addClass('minimized')
-  }
-}])
+    $scope.transition = function(){
+        $('.menu').addClass('minimized')
+    }
+}]);
